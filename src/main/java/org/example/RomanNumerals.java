e6349a46f397945175cdba8d51d411a45219fd6e
Javi <javiermespino@gmail.com>
2023-03-24 13:42:05 +0000

    Test(green): number_four_should_return_IV

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class RomanNumerals {
     public static String transform(int number) {
-        return "I".repeat(number);
+        return number < 4 ? "I".repeat(number) : "IV";
     }
 }
